56325bef6fe12db29a967fddb3d447bc9b7e4146
Steven Pilatschek <steven.pilatschek@gmail.com>
2018-03-12 10:25:50 +0100

    Implemented commenting
    implemented commenting on blogposts according to assessment

--- Blog/urls.py
+++ Blog/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('posts/', views.PostListView.as_view(), name="posts"),
     path('posts/<int:pk>', views.post_detail_view, name="post-detail"),
+    path('posts/<int:pk>/add', views.add_comment_form, name="comment-add"),
+    path('posts/<int:pk>/do-add', views.add_comment, name="comment-add-do"),
     path('users/', views.UserListView.as_view(), name="users"),
     path('user/<int:pk>', views.user_detail_view, name="user-detail"),
-
 ]
 
--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,9 @@
+import datetime
+
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render
+
 from .models import Post, Comment
 
 
@@ -22,6 +26,28 @@ class UserListView(generic.ListView):
     template_name = 'Blog/user_list.html'
 
 
+@login_required
+def add_comment_form(request, pk):
+    post = Post.objects.get(pk=pk)
+    do_add_url = request.build_absolute_uri().replace('add', 'do-add')
+    context = {'blogpost': post, 'do_add_url': do_add_url, }
+    return render(request, 'Blog/comment_add.html', context)
+
+
+@login_required()
+def add_comment(request, pk):
+    if request.method == "POST":
+        content = request.POST.get("content", "")
+        comment = Comment()
+        comment.user = request.user
+        comment.post = Post.objects.get(pk=pk)
+        comment.created = datetime.datetime.now()
+        comment.content = content
+        comment.save()
+        return post_detail_view(request, pk)
+    return index(request)
+
+
 def user_detail_view(request, pk):
     user = User.objects.get(pk=pk)
     posts = Post.objects.filter(user=user)
@@ -32,6 +58,7 @@ def user_detail_view(request, pk):
 def post_detail_view(request, pk):
     post = Post.objects.get(pk=pk)
     comments = Comment.objects.filter(post=post)
-    context = {'post': post, 'comments': comments}
+    comment_add_url = "{}/add".format(request.get_full_path())
+    context = {'post': post, 'comments': comments, 'comment_url': comment_add_url}
     return render(request, 'Blog/post_detail.html', context)
 
