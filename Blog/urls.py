426851edbf7a568ddb271ec4656346a254052816
Steven Pilatschek <steven.pilatschek@gmail.com>
2018-05-14 08:42:12 +0200

    removed settings page
    since this page is not used anymore in the redesigned website

--- Blog/urls.py
+++ Blog/urls.py
@@ -18,7 +18,6 @@ urlpatterns = [
     path('user/<username>', views.user_detail_view, name="user-detail"),
     path('profile/edit', views.update_profile, name="profile-update"),
     path('latest/feed/', LatestPostsFeed(), name="feed"),
-    path('settings/', views.settings, name="settings"),
     url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
