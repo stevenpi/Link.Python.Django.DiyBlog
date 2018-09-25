af07efe630dc10bcab405c1b8b1b9ae8241b70db
Steven Pilatschek <steven.pilatschek@gmail.com>
2018-09-25 12:47:54 +0200

    Implemented Schema generation

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.urls import include
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from api import views
 
@@ -11,6 +12,9 @@ router.register(r'posts', views.PostViewSet)
 router.register(r'users', views.UserViewSet)
 router.register(r'comments', views.CommentViewSet)
 
+schema_view = get_schema_view(title='Pastebin API')
+
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^', include(router.urls))
 ]
